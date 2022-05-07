fe4f1fda757a0a6ae20b8db9bbca76a967c58506
Neeraj Malkani <neerajmalkani33@gmail.com>
2022-05-08 03:37:52 +0530

    Left menu fixes

--- App.js
+++ App.js
@@ -1,38 +1,77 @@
 import { Provider as PaperProvider, List, Divider } from "react-native-paper";
 import { theme } from "./src/theme/apptheme";
 import "react-native-gesture-handler";
-import { NavigationContainer, createNavigationContainerRef } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import MasterScreen from "./src/screens/Master.screen";
 import DashboardScreen from "./src/screens/Dashboard.screen";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Container } from "./src/components/container";
-import { MenuItems } from "./src/menu/MenuItems";
+import { MenuItems, navigationRef } from "./src/menu/MenuItems";
+import { ScrollView } from "react-native";
+import { useState } from "react";
 
-export const navigationRef = createNavigationContainerRef();
 const Drawer = createDrawerNavigator();
 
-const DrawerContent = () => {
-  return (
-    <Container>
-      <List.Section>
-        {MenuItems.map((k, i) => {
-          return k.list === undefined ? (
-            <List.Item key={i} title={k.title} style={{ paddingVertical: 0, borderBottomWidth: 1, borderBottomColor: "#ededed" }} left={k.icon} />
-          ) : (
-            <List.Accordion key={i} title={k.title} style={{ backgroundColor: "white", paddingVertical: 0, borderBottomWidth: 1, borderBottomColor: "#ededed" }} left={k.icon}>
-              {k.list.map((l, j) => {
-                return <List.Item key={j} title={l.title} style={{ backgroundColor: "#ededed", height: 48, borderBottomWidth: 1, borderBottomColor: "#dedede" }} />;
-              })}
-            </List.Accordion>
-          );
-        })}
-      </List.Section>
-    </Container>
-  );
-};
-
 export default function App() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const DrawerContent = () => {
+    return (
+      <ScrollView>
+        <Container>
+          <List.Section>
+            {MenuItems.map((k, i) => {
+              return k.list === undefined ? (
+                <List.Item
+                  key={i}
+                  titleStyle={{ color: parseInt(i) === activeIndex ? theme.colors.primary : theme.colors.text }}
+                  title={k.title}
+                  style={{ paddingVertical: 0, borderBottomWidth: 1, borderBottomColor: "#ededed" }}
+                  left={() => <List.Icon color={parseInt(i) === activeIndex ? theme.colors.primary : theme.colors.textLightSecondary} icon={k.icon} />}
+                  onPress={(e) => {
+                    if (k.navigation !== undefined) {
+                      navigationRef.navigate(k.navigation);
+                    }
+                    if (k.index !== undefined) {
+                      setActiveIndex(k.index);
+                    }
+                  }}
+                />
+              ) : (
+                <List.Accordion
+                  key={i}
+                  title={k.title}
+                  id={i}
+                  titleStyle={{ color: parseInt(i) === activeIndex ? theme.colors.primary : theme.colors.text }}
+                  style={{ backgroundColor: "white", paddingVertical: 0, borderBottomWidth: 1, borderBottomColor: "#ededed" }}
+                  left={() => <List.Icon color={parseInt(i) === activeIndex ? theme.colors.primary : theme.colors.textLightSecondary} icon={k.icon} />}
+                >
+                  {k.list.map((l, j) => {
+                    return (
+                      <List.Item
+                        key={j}
+                        title={l.title}
+                        titleStyle={{ color: parseInt(i.toString() + j.toString()) === activeIndex ? theme.colors.primary : theme.colors.text }}
+                        style={{ backgroundColor: "#ededed", height: 52, borderBottomWidth: 1, borderBottomColor: "#dedede" }}
+                        onPress={() => {
+                          if (l.navigation !== undefined) {
+                            navigationRef.navigate(l.navigation);
+                          }
+                          if (l.index !== undefined) {
+                            setActiveIndex(l.index);
+                          }
+                        }}
+                      />
+                    );
+                  })}
+                </List.Accordion>
+              );
+            })}
+          </List.Section>
+        </Container>
+      </ScrollView>
+    );
+  };
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <PaperProvider theme={theme}>
--- src/menu/MenuItems.js
+++ src/menu/MenuItems.js
@@ -1,52 +1,80 @@
 import { List } from "react-native-paper";
+import { createNavigationContainerRef } from "@react-navigation/native";
+
+export const navigationRef = createNavigationContainerRef();
 
 export const MenuItems = [
   {
     title: "Dashboard",
-    icon: () => <List.Icon color="#7d7d7d" icon={"view-dashboard"} />,
+    icon: "view-dashboard",
+    navigation: "Dashboard",
+    index: 0,
   },
   {
     title: "Master",
-    icon: () => <List.Icon color="#7d7d7d" icon={"plus-thick"} />,
+    icon: "plus-thick",
+    index: 1,
     list: [
       {
         title: "Raw Material Brand",
+        navigation: "Master",
+        index: 10,
       },
       {
         title: "Width of GP coil",
+        index: 11,
       },
       {
         title: "Mass of Zinc coating",
+        index: 12,
       },
       {
         title: "Product for Production",
+        index: 13,
       },
       {
         title: "Vendor",
+        index: 14,
       },
       {
         title: "Supplier",
+        index: 15,
       },
     ],
   },
   {
     title: "Purchase Order",
-    icon: () => <List.Icon color="#7d7d7d" icon={"file-document-multiple"} />,
+    icon: "file-document-multiple",
+    index: 2,
   },
   {
-    title: "Vendor Order / Slitting Details",
-    icon: () => <List.Icon color="#7d7d7d" icon={"store"} />,
+    title: "Vendor Order", // / Slitting Details
+    icon: "store",
+    index: 3,
   },
   {
     title: "Invoice receipt",
-    icon: () => <List.Icon color="#7d7d7d" icon={"receipt"} />,
+    icon: "receipt",
+    index: 4,
   },
   {
     title: "Production",
-    icon: () => <List.Icon color="#7d7d7d" icon={"factory"} />,
+    icon: "factory",
+    index: 5,
+    list: [
+      {
+        title: "Add Production",
+        index: 50,
+      },
+      {
+        title: "Production List",
+        index: 51,
+      },
+    ],
   },
   {
     title: "Sales Order Form",
-    icon: () => <List.Icon color="#7d7d7d" icon={"point-of-sale"} />,
+    icon: "point-of-sale",
+    index: 6,
   },
 ];
