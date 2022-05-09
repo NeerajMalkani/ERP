3c451ac22718c6b555595ab28ab9013addf7d5bb
Neeraj Malkani <neerajmalkani33@gmail.com>
2022-05-09 19:23:41 +0530

    Fixes

--- src/screens/AddRawMaterial.js
+++ src/screens/AddRawMaterial.js
@@ -1,28 +1,95 @@
+import { useState } from "react";
 import { Card, TextInput } from "react-native-paper";
-import { View } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { Box } from "../components/box";
 import RNPickerSelect from "react-native-picker-select";
+import { Dropdown } from "react-native-element-dropdown";
 import { theme } from "../theme/apptheme";
+import AntDesign from "react-native-vector-icons/AntDesign";
 
 export default AddRawMaterial = () => {
+  const [value, setValue] = useState(null);
+  const [isFocus, setIsFocus] = useState(false);
+  const data = [
+    { label: "Item 1", value: "1" },
+    { label: "Item 2", value: "2" },
+    { label: "Item 3", value: "3" },
+    { label: "Item 4", value: "4" },
+    { label: "Item 5", value: "5" },
+    { label: "Item 6", value: "6" },
+    { label: "Item 7", value: "7" },
+    { label: "Item 8", value: "8" },
+  ];
+  const renderLabel = () => {
+    if (value || isFocus) {
+      return <Text style={[styles.label, isFocus && { color: "blue" }]}>Dropdown label</Text>;
+    }
+    return null;
+  };
+
   return (
-    <Box style={{ flex: 1 }}>
-      <Card>
-        <Card.Content>
-          <View style={{ padding: 16 }}>
-            <RNPickerSelect
-              onValueChange={(value) => console.log(value)}
-              useNativeAndroidPickerStyle={true}
-              items={[
-                { label: "Football", value: "football" },
-                { label: "Baseball", value: "baseball" },
-                { label: "Hockey", value: "hockey" },
-              ]}
-            />
-            {/* <TextInput label="Email" onChangeText={{}} /> */}
-          </View>
-        </Card.Content>
-      </Card>
-    </Box>
+    <View style={styles.container}>
+      {renderLabel()}
+      <Dropdown
+        style={[styles.dropdown, isFocus && { borderColor: "blue" }]}
+        placeholderStyle={styles.placeholderStyle}
+        selectedTextStyle={styles.selectedTextStyle}
+        iconStyle={styles.iconStyle}
+        data={data}
+        maxHeight={300}
+        labelField="label"
+        valueField="value"
+        dropdownPosition="auto"
+        placeholder={!isFocus ? "Select item" : ""}
+        value={value}
+        onFocus={() => setIsFocus(true)}
+        onBlur={() => setIsFocus(false)}
+        onChange={(item) => {
+          setValue(item.value);
+          setIsFocus(false);
+        }}
+      />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 16,
+    flex: 1
+  },
+  dropdown: {
+    height: 48,
+    borderColor: "gray",
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  label: {
+    position: "absolute",
+    backgroundColor: "white",
+    left: 22,
+    top: 8,
+    zIndex: 999,
+    paddingHorizontal: 8,
+    fontSize: 14,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+});
